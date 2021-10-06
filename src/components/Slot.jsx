a891f3f546139bad475c0b3f217d2f13ff04854f
Sebastian Garcia <bashgar.dev@gmail.com>
2021-10-06 17:50:38 -0400

    refactored some sections, added comments, finished perk_2 selection

--- src/components/Slot.jsx
+++ src/components/Slot.jsx
@@ -3,6 +3,7 @@ import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
 import db from "../db/db.json";
 import properties from "../db/properties.json";
+import perks from "../db/perks.json";
 import items from "../db/items.json";
 import "../styles/themes/dark.css";
 // import Ki from "./Ki";
@@ -22,15 +23,15 @@ export default function Slot(props) {
     .filter((item) => item.gear_type === slotType)
     .map((item) => item);
 
-  // Determine properties compatible with the item selected
-  // const compatibleProperties = properties;
-
+  // Item information assignment
   const itemInfo = items.filter(
     (item) => item.id === currentLoadout[slotType].item
   )[0];
   const title = itemInfo.title;
   const rarity = itemInfo.rarity;
   const icon = `/images/${itemInfo.id}.jpg`;
+
+  // Property 1 assignments
   const property_1_title =
     db.properties[currentLoadout[slotType].property_1].title;
   const property_1_values = `+ ${
@@ -43,6 +44,8 @@ export default function Slot(props) {
   }`;
   const property_1_description =
     db.properties[currentLoadout[slotType].property_1].description;
+
+  // Property 2 assignments
   const property_2_title =
     db.properties[currentLoadout[slotType].property_2].title;
   const property_2_values = `+ ${
@@ -55,15 +58,16 @@ export default function Slot(props) {
   }`;
   const property_2_description =
     db.properties[currentLoadout[slotType].property_2].description;
+
+  // Perk 1 assignments
   const perk_1_title = db.perks[currentLoadout[slotType].perk_1].title;
   const perk_1_description =
     db.perks[currentLoadout[slotType].perk_1].description;
-  let perk_2_title = "-";
-  let perk_2_description = "No perk selected";
-  if (currentLoadout[slotType].perk_2) {
-    perk_2_title = db.perks[currentLoadout[slotType].perk_2].title;
-    perk_2_description = db.perks[currentLoadout[slotType].perk_2].description;
-  }
+
+  // Perk 2 assignments
+  const perk_2_title = db.perks[currentLoadout[slotType].perk_2].title;
+  const perk_2_description =
+    db.perks[currentLoadout[slotType].perk_2].description;
 
   return (
     <div id="" className=" grid md:grid-cols-2   ">
@@ -78,10 +82,11 @@ export default function Slot(props) {
             alt=""
           ></img>
         </div>
-        <div className="self-center mr-4 text-xl ">
-          <div className="items-center "></div>
-          {title}
 
+        {/* Item Title Section*/}
+
+        <div className="self-center mr-4 text-xl ">
+          {title}
           <Listbox
             as="div"
             className="absolute top-0 right-0"
@@ -120,6 +125,8 @@ export default function Slot(props) {
         </div>
       </div>
 
+      {/* Properties and Perks Section */}
+
       <div className="md:border-t-2 border-white border-opacity-30 text-white bg-black bg-opacity-50 pl-4 py-2 flex justify-between">
         <div className="table w-full">
           <div className="table-row-group text-white text-opacity-80">
@@ -145,7 +152,6 @@ export default function Slot(props) {
                   value={currentLoadout[slotType].property_1}
                   onChange={(value) =>
                     setCurrentLoadout((prevState) => {
-                      console.log(currentLoadout[slotType]);
                       prevState[slotType].property_1 = value.id;
                       return { ...prevState };
                     })
@@ -195,7 +201,6 @@ export default function Slot(props) {
                   value={currentLoadout[slotType].property_2}
                   onChange={(value) =>
                     setCurrentLoadout((prevState) => {
-                      console.log(currentLoadout[slotType]);
                       prevState[slotType].property_2 = value.id;
                       return { ...prevState };
                     })
@@ -223,8 +228,8 @@ export default function Slot(props) {
               </div>
             </div>
 
-            <div className="table-row ">
-              <div className="table-cell text-red-600 whitespace-nowrap">
+            <div className="table-row text-red-600">
+              <div className="table-cell whitespace-nowrap">
                 <Tippy
                   className="p-1 font-urbanist font-medium"
                   placement="bottom"
@@ -233,15 +238,16 @@ export default function Slot(props) {
                 >
                   <span>{perk_1_title}</span>
                 </Tippy>
-                <SelectorPropertyButton />
-                {/* <Listbox
+              </div>
+              <div className="table-cell text-right whitespace-nowrap">-</div>
+              <div className="table-cell text-right whitespace-nowrap">
+                <Listbox
                   as="div"
-                  className="relative top-0 right-0"
-                  value={currentLoadout[slotType].property_2}
+                  className="relative text-right pr-2"
+                  value={currentLoadout[slotType].perk_1}
                   onChange={(value) =>
                     setCurrentLoadout((prevState) => {
-                      console.log(currentLoadout[slotType]);
-                      prevState[slotType].property_2 = value.id;
+                      prevState[slotType].perk_1 = value.id;
                       return { ...prevState };
                     })
                   }
@@ -251,20 +257,20 @@ export default function Slot(props) {
                   </Listbox.Button>
 
                   <Listbox.Options className=" absolute top-10 right-10 bg-black shadow-lg p-0.5 rounded-lg z-10 text-base">
-                    {properties.map((property) => (
-                      <Listbox.Option key={property.id} value={property}>
+                    {perks.map((perk) => (
+                      <Listbox.Option key={perk.id} value={perk}>
                         <div className="whitespace-nowrap flex justify-between bg-white m-0.5 px-2 py-1 rounded-md bg-opacity-10">
-                          {property.title}
+                          {perk.title}
                         </div>
                       </Listbox.Option>
                     ))}
                   </Listbox.Options>
-                </Listbox> */}
+                </Listbox>
               </div>
             </div>
 
-            <div className="table-row">
-              <div className="table-cell text-red-600">
+            <div className="table-row text-red-600">
+              <div className="table-cell whitespace-nowrap">
                 <Tippy
                   className="p-1 font-urbanist font-medium"
                   placement="bottom"
@@ -273,7 +279,34 @@ export default function Slot(props) {
                 >
                   <span>{perk_2_title}</span>
                 </Tippy>
-                <SelectorPropertyButton />
+              </div>
+              <div className="table-cell text-right whitespace-nowrap">-</div>
+              <div className="table-cell text-right whitespace-nowrap">
+                <Listbox
+                  as="div"
+                  className="relative text-right pr-2"
+                  value={currentLoadout[slotType].perk_2}
+                  onChange={(value) =>
+                    setCurrentLoadout((prevState) => {
+                      prevState[slotType].perk_2 = value.id;
+                      return { ...prevState };
+                    })
+                  }
+                >
+                  <Listbox.Button>
+                    <SelectorPropertyButton />
+                  </Listbox.Button>
+
+                  <Listbox.Options className=" absolute top-10 right-10 bg-black shadow-lg p-0.5 rounded-lg z-10 text-base">
+                    {perks.map((perk) => (
+                      <Listbox.Option key={perk.id} value={perk}>
+                        <div className="whitespace-nowrap flex justify-between bg-white m-0.5 px-2 py-1 rounded-md bg-opacity-10">
+                          {perk.title}
+                        </div>
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Listbox>
               </div>
             </div>
           </div>
