13224074c80e6bb683a2261cb500a6901fcf0ec9
Sebastian Garcia <bashgar.dev@gmail.com>
2021-10-06 17:49:13 -0400

    moved blueprint logic to LoadoutContext
    -modified Loadout.jsx
    -removed CurrentLoadoutJSON (debug purposes)

--- src/components/Loadout.jsx
+++ src/components/Loadout.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Build from "./Build";
-import CurrentLoadoutJSON from "./CurrentLoadoutJSON";
+// import CurrentLoadoutJSON from "./CurrentLoadoutJSON";
+import Blueprint from "./Blueprint";
 
 import Hero from "./Hero";
+import Techniques from "./Techniques";
 
 export default function Loadout() {
   return (
     <>
       <div className="flex-1 flex-shrink-0 max-w-screen-md shadow-2xl md:border-2 md:border-black md:border-opacity-10 md:rounded-2xl md:overflow-hidden">
         <Hero />
+        <Techniques />
+
         <Build />
-      </div>
-      <div className="flex-1 overflow-x-auto">
-        <CurrentLoadoutJSON />
+        <Blueprint />
       </div>
     </>
   );
--- src/context/LoadoutContext.jsx
+++ src/context/LoadoutContext.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import loadout from "../db/loadout.json";
 import zlib from "zlib";
 import base64url from "base64url";
@@ -7,28 +7,45 @@ import base64url from "base64url";
 export const LoadoutContext = createContext();
 
 export const LoadoutContextProvider = ({ children }) => {
-  const queryParams = new URLSearchParams(window.location.search);
-  const blueprint = queryParams.get("blueprint");
   let initialState = loadout;
 
-  if (blueprint) {
-    initialState = readBlueprint(blueprint);
+  const queryParams = new URLSearchParams(window.location.search);
+  const blueprintUrl = queryParams.get("blueprint");
+
+  if (blueprintUrl) {
+    initialState = readBlueprint(blueprintUrl);
   }
 
+  const [blueprint, setBlueprint] = useState(makeBlueprint(initialState));
   const [currentLoadout, setCurrentLoadout] = useState(initialState);
+
+  function makeBlueprint(data) {
+    return base64url.encode(
+      zlib.deflateSync(JSON.stringify(data), { level: 9 })
+    );
+  }
 
   function readBlueprint(data) {
     return JSON.parse(zlib.inflateSync(base64url.toBuffer(data)).toString());
   }
+
+  useEffect(() => {
+    setBlueprint(makeBlueprint(currentLoadout));
+  }, [currentLoadout]);
 
   return (
     <LoadoutContext.Provider
       value={{
         currentLoadout,
         setCurrentLoadout,
+        blueprint,
+        setBlueprint,
+        readBlueprint,
+        makeBlueprint,
       }}
     >
       {children}
+      {console.log(currentLoadout)}
     </LoadoutContext.Provider>
   );
 };
